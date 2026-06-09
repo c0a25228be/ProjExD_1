571dae40d3c185529d78d33f2350fa3e43974381
10N <c0a25228be@edu.teu.ac.jp>
2026-06-09 16:40:09 +0900

    演習課題２：move_ipメソッドの使用を一回に変更

--- pygame_image.py
+++ pygame_image.py
@@ -21,16 +21,20 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
-        kk_rct.move_ip(-1, 0)
+
+        move_x = -1
+        move_y = 0
 
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
+            move_y = -1
         if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, +1))
+            move_y = +1
         if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))
+            move_x = -2
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+2, 0))
+            move_x = +1
+
+        kk_rct.move_ip(move_x, move_y)
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0]) #背景を左にずらして描画
