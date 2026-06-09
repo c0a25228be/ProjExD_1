0999d9908ce5ae405b6f8ef1066dd32913e84455
10N <c0a25228be@edu.teu.ac.jp>
2026-06-09 16:36:33 +0900

    演習課題１：こうかとんを背景画像と同じ速度で左に流れるようにした

--- pygame_image.py
+++ pygame_image.py
@@ -21,6 +21,8 @@ def main():
             if event.type == pg.QUIT: return
 
         key_lst = pg.key.get_pressed()
+        kk_rct.move_ip(-1, 0)
+
         if key_lst[pg.K_UP]:
             kk_rct.move_ip((0, -1))
         if key_lst[pg.K_DOWN]:
@@ -28,7 +30,7 @@ def main():
         if key_lst[pg.K_LEFT]:
             kk_rct.move_ip((-1, 0))
         if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((+1, 0))
+            kk_rct.move_ip((+2, 0))
 
         x=tmr%3200
         screen.blit(bg_img, [-x, 0]) #背景を左にずらして描画
